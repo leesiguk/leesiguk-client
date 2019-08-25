47f266a5526ab927cd7fae55c5bfcb4aca81b8a8
youngjin <masichall@gmail.com>
2019-08-25 23:45:45 +0900

    sns 공유기능 추가

--- src/components/home/MatchCard.vue
+++ src/components/home/MatchCard.vue
@@ -6,9 +6,30 @@
         >
             <v-toolbar-title>{{data.category}}</v-toolbar-title>
             <div class="flex-grow-1"></div>
-            <v-btn icon>
-                <v-icon class="material-icons-outlined">share</v-icon>
-            </v-btn>
+            <v-menu bottom left>
+                <template v-slot:activator="{ on }">
+                    <v-btn
+                            icon
+                            v-on="on"
+                    >
+                        <v-icon class="material-icons-outlined">share</v-icon>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item @click="shareKakao()">
+                        <v-list-item-avatar>
+                            <v-img src="/img/kakaotalk.png"></v-img>
+                        </v-list-item-avatar>
+                        <v-list-item-title>카카오톡</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item @click="shareFacebook()">
+                        <v-list-item-avatar>
+                            <v-img src="/img/facebook.png"></v-img>
+                        </v-list-item-avatar>
+                        <v-list-item-title>Facebook</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </v-app-bar>
         <v-divider></v-divider>
         <match-card-main-content :data="data"></match-card-main-content>
@@ -26,11 +47,46 @@
     import MatchCardMainContent from '@/components/home/MatchCardMainContent.vue';
     import MatchCardCurrentLikeChart from '@/components/home/MatchCardCurrentLikeChart.vue';
 
+    declare const Kakao: any;
+    declare const FB: any;
+
     @Component({
         components: {MatchCardCurrentLikeChart, MatchCardMainContent},
     })
     export default class MatchCard extends Vue {
         @Prop() data: any;
+
+        shareFacebook() {
+            FB.ui({
+                method: 'share_open_graph',
+                action_type: 'og.likes',
+                action_properties: JSON.stringify({
+                    object: 'https://www.leesiguk.com',
+                    title: `${this.data.japan.name} vs ${this.data.korea.name} 과연 승자는?`,
+                    image: 'https://www.leesiguk.com/img/android-chrome-192x192.png',
+                }),
+            });
+        }
+
+        shareKakao() {
+            Kakao.Link.sendDefault({
+                objectType: 'text',
+                text: `${this.data.japan.name} vs ${this.data.korea.name}\n과연 승자는?`,
+                link: {
+                    mobileWebUrl: 'https://www.leesiguk.com',
+                    webUrl: 'https://www.leesiguk.com',
+                },
+                buttons: [
+                    {
+                        title: '대결 상황 확인하기',
+                        link: {
+                            mobileWebUrl: 'https://www.leesiguk.com',
+                            webUrl: 'https://www.leesiguk.com',
+                        },
+                    },
+                ],
+            });
+        }
     }
 </script>
 
