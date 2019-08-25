fa1fb37db947a052840d112fe561a560d0579168
youngjin <masichall@gmail.com>
2019-08-25 19:53:23 +0900

    데이터 하드코딩

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,9 +39,78 @@
                 like: 332065,
                 facebookPageUrl: 'https://www.facebook.com/SPAOSTORY/',
             },
+        }, {
+            id: 'b',
+            category: '편의점',
+            japan: {
+                name: '미니스톱',
+                brandImageUrl: '/img/brands/ministop.jpg',
+                brandColor: '#ffffff',
+                like: 125502,
+                facebookPageUrl: 'https://www.facebook.com/ministopkr/',
+            },
+            korea: {
+                name: '이마트24',
+                brandImageUrl: '/img/brands/emart24.png',
+                brandColor: '#ffffff',
+                like: 58434,
+                facebookPageUrl: 'https://www.facebook.com/emart24.official',
+            },
+        }, {
+            id: 'c',
+            category: '슈퍼 멀티 스토어',
+            japan: {
+                name: '에이비씨마트',
+                brandImageUrl: '/img/brands/abcmart.jpg',
+                brandColor: '#FFDD00',
+                like: 213672,
+                facebookPageUrl: 'https://www.facebook.com/abcmartkorea/',
+            },
+            korea: {
+                name: '레스모아',
+                brandImageUrl: '/img/brands/lesmore.jpg',
+                brandColor: '#ffffff',
+                like: 102284,
+                facebookPageUrl: 'https://www.facebook.com/lesmore.official',
+            },
+        }, {
+            id: 'd',
+            category: '시계',
+            japan: {
+                name: 'G-CHOCK',
+                brandImageUrl: '/img/brands/gshock.png',
+                brandColor: '#000000',
+                like: 105874,
+                facebookPageUrl: 'https://www.facebook.com/koreagshock/',
+            },
+            korea: {
+                name: 'DOLPHIN',
+                brandImageUrl: '/img/brands/dolphinwatch.png',
+                brandColor: '#ffffff',
+                like: 29,
+                facebookPageUrl: 'https://www.facebook.com/DolphinWatch',
+            },
+        }, {
+            id: 'e',
+            category: '생활용품',
+            japan: {
+                name: 'MUJI',
+                brandImageUrl: '/img/brands/muji.jpg',
+                brandColor: '#970A2C',
+                like: 104532,
+                facebookPageUrl: 'https://www.facebook.com/mujikorea/',
+            },
+            korea: {
+                name: 'JAJU',
+                brandImageUrl: '/img/brands/jaju.jpg',
+                brandColor: '#58585A',
+                like: 65923,
+                facebookPageUrl: 'https://www.facebook.com/shinsegaejaju',
+            },
         }];
     }
 </script>
 
 <style scoped lang="scss">
+
 </style>
