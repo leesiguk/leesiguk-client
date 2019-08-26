f59b7fbada6ca1306c459d7de6329dbdd1f27201
youngjin <masichall@gmail.com>
2019-08-26 11:06:51 +0900

    routes 개선

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,10 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
         {
+            path: '*',
+            redirect: '/',
+        },
+        {
             path: '/',
             name: 'home',
             component: Home,
